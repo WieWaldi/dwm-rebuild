09abb4117ab40ff3b54bea028890211283fd2da3
Waldemar Schröer <w@ldemar.de>
2024-10-28 14:33:24 +0100

    Minor changes

--- config.def.h
+++ config.def.h
@@ -1,28 +1,51 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int focusedontoptiled  = 1;        /* 1 means focused tile client is shown on top of floating windows */
-static const int focusonwheel       = 0;
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const unsigned int borderpx          = 1;        /* border pixel of windows */
+static const unsigned int snap              = 32;       /* snap pixel */
+static const unsigned int gappih            = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv            = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh            = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov            = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps                  = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar                    = 1;        /* 0 means no bar */
+static const int topbar                     = 1;        /* 0 means bottom bar */
+static const int focusedontoptiled          = 1;        /* 1 means focused tile client is shown on top of floating windows */
+static const int focusonwheel               = 0;
+static const char *fonts[]                  = { "FiraMono Nerd Font:size=12", "monospace:size=10" };
+static const char dmenufont[]               = "monospace:size=10";
+static const char col_gray1[]               = "#222222";
+static const char col_gray2[]               = "#444444";
+static const char col_gray3[]               = "#bbbbbb";
+static const char col_gray4[]               = "#eeeeee";
+static const char col_cyan[]                = "#005577";
+
+//static char col_normbgcolor[]         = "#5f005f";
+//static char col_normbordercolor[]     = "#444444";
+//static char col_normfgcolor[]         = "#bcbcbc";
+//static char col_selfgcolor[]          = "#eeeeee";
+//static char col_selbordercolor[]      = "#000000";
+//static char col_selbgcolor[]          = "#000000";
+//static char col_statusfgcolor[]       = "#eeeeee";
+//static char col_statusbgcolor[]       = "#5f005f";
+//static char col_tagselfgcolor[]       = "#eeeeee";
+//static char col_tagselbgcolor[]       = "#5f005f";
+//static char col_tagnormfgcolor[]      = "#af87d7";
+//static char col_tagnormbgcolor[]      = "#5f005f";
+//static char col_infoselfgcolor[]      = "#eeeeee";
+//static char col_infoselbgcolor[]      = "#5f005f";
+//static char col_infonormfgcolor[]     = "#eeeeee";
+//static char col_infonormbgcolor[]     = "#5f005f";
+
+static const char *colors[][3] = {
+		/*                                      fg                  bg                  border   */
+		[SchemeNorm]                        = { "#bcbcbc",        "#5f005f",        "#444444" },
+		[SchemeSel]                         = { "#eeeeee",         "#000000",         "#000000"  },
+		[SchemeStatus]                      = { "#eeeeee",      "#5f005f",      "#000000"  },           // Statusbar right {text,background,not used but cannot be empty}
+		[SchemeTagsSel]                     = { "#eeeeee",      "#5f005f",      "#000000"  },           // Tagbar left selected {text,background,not used but cannot be empty}
+		[SchemeTagsNorm]                    = { "#af87d7",     "#5f005f",     "#000000"  },           // Tagbar left unselected {text,background,not used but cannot be empty}
+		[SchemeInfoSel]                     = { "#eeeeee",     "#5f005f",     "#000000"  },           // infobar middle  selected {text,background,not used but cannot be empty}
+		[SchemeInfoNorm]                    = { "#eeeeee",    "#5f005f",    "#000000"  },           // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
