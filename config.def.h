8cb47128987080fd8b475bd29aa672c4b2c9197e
Waldemar Schröer <w@ldemar.de>
2024-11-05 23:05:37 +0100

    patch with 'dwm-xresources-20210827-138b405.diff'.

--- config.def.h
+++ config.def.h
@@ -1,26 +1,38 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx          = 1;        /* border pixel of windows */
-static const unsigned int snap              = 32;       /* snap pixel */
+// static const unsigned int borderpx          = 1;        /* border pixel of windows */
+// static const unsigned int snap              = 32;       /* snap pixel */
 static const unsigned int gappih            = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv            = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh            = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov            = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps                  = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar                    = 1;        /* 0 means no bar */
-static const int topbar                     = 1;        /* 0 means bottom bar */
+// static const int showbar                    = 1;        /* 0 means no bar */
+// static const int topbar                     = 1;        /* 0 means bottom bar */
 static const int focusedontoptiled          = 1;        /* 1 means focused tile client is shown on top of floating windows */
 static const int focusonwheel               = 0;
-static const char *fonts[]                  = { "FiraMono Nerd Font:size=12", "monospace:size=10" };
-static const char dmenufont[]               = "monospace:size=10";
-static const char col_gray1[]               = "#222222";
-static const char col_gray2[]               = "#444444";
-static const char col_gray3[]               = "#bbbbbb";
-static const char col_gray4[]               = "#eeeeee";
-static const char col_cyan[]                = "#005577";
-
-static const char *colors[][3] = {
+// static const char *fonts[]                  = { "FiraMono Nerd Font:size=12", "monospace:size=10" };
+// static const char dmenufont[]               = "monospace:size=10";
+// static const char col_gray1[]               = "#222222";
+// static const char col_gray2[]               = "#444444";
+// static const char col_gray3[]               = "#bbbbbb";
+// static const char col_gray4[]               = "#eeeeee";
+// static const char col_cyan[]                = "#005577";
+static unsigned int borderpx  = 1;        /* border pixel of windows */
+static unsigned int snap      = 32;       /* snap pixel */
+static int showbar            = 1;        /* 0 means no bar */
+static int topbar             = 1;        /* 0 means bottom bar */
+static char font[]            = "monospace:size=10";
+static char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { font };
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
 		/*                                         fg          bg      border   */
 		[SchemeNorm]                        = { "#bcbcbc", "#5f005f", "#444444" },
 		[SchemeSel]                         = { "#eeeeee", "#000000", "#000000" },
@@ -30,6 +42,12 @@ static const char *colors[][3] = {
 		[SchemeInfoSel]                     = { "#eeeeee", "#5f005f", "#000000" },  // infobar middle  selected {text,background,not used but cannot be empty}
 		[SchemeInfoNorm]                    = { "#eeeeee", "#5f005f", "#000000" },  // infobar middle  unselected {text,background,not used but cannot be empty}
 };
+
+// static char *colors[][3] = {
+//        /*               fg           bg           border   */
+//        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+//        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+//  };
 
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
@@ -92,9 +110,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
+static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
@@ -136,7 +154,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0";                                                                                  // component of dmenucmd, manipulated in spawn()
-static const char *dmenucmd[]           = { "dmenu_run", "-m", dmenumon, "-i", "-c", "-l", "15", "-bw", "2", "-p", "Яцп ТЋїѕ Ѕћїт: 󰜎 ", NULL };
+static const char *dmenucmd[]           = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *dmenusystem[]        = { "dwm-system", NULL };
 static const char *dmenumedia[]         = { "dwm-media", NULL };
 static const char *termcmd[]            = { "st", NULL };
@@ -164,6 +182,27 @@ static const StatusCmd statuscmds[]     = {
                                             { "dwm-statuscmd 7 $BUTTON", 7 },
                                             { "dwm-statuscmd 8 $BUTTON", 8 },
                                             { "dwm-statuscmd 9 $BUTTON", 9 },
+};
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "font",               STRING,  &font },
+		{ "dmenufont",          STRING,  &dmenufont },
+		{ "normbgcolor",        STRING,  &normbgcolor },
+		{ "normbordercolor",    STRING,  &normbordercolor },
+		{ "normfgcolor",        STRING,  &normfgcolor },
+		{ "selbgcolor",         STRING,  &selbgcolor },
+		{ "selbordercolor",     STRING,  &selbordercolor },
+		{ "selfgcolor",         STRING,  &selfgcolor },
+		{ "borderpx",          	INTEGER, &borderpx },
+		{ "snap",          		INTEGER, &snap },
+		{ "showbar",          	INTEGER, &showbar },
+		{ "topbar",          	INTEGER, &topbar },
+		{ "nmaster",          	INTEGER, &nmaster },
+		{ "resizehints",       	INTEGER, &resizehints },
+		{ "mfact",      	 	FLOAT,   &mfact },
 };
 
 static const Key keys[] = {
